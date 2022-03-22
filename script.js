d5b63845cdecfac288102a4b9fdd2de5f8af0226
Aron Adams <aronrapetto@hotmail.com>
2022-03-22 16:22:28 -0300

    requisito 8 corrigido

--- script.js
+++ script.js
@@ -5,29 +5,15 @@ function armazenaTarefa(texto) {
   listaDeTarefas.push(texto);
 }
 
-// function validaSelected() {
-//   const tarefas = document.getElementsByClassName('tarefas');
-//   let contSelected = 0;
-//   for (let i = 0; i < tarefas.length; i += 1) {
-//     if (tarefas[i].classList.contains('selected')) {
-//       contSelected += 1;
-//     }
-//   }
-//   console.log(contSelected);
-//   return contSelected;
-// }
-
 function selecionaTarefa(event) {
   const tarefaSelecionada = document.getElementsByClassName('selected');
 
-  if (event.target.classList.contains('selected')) {
-    event.target.classList.remove('selected');
-  } else if (tarefaSelecionada.length >= 1) {
-    alert('Apenas uma tarefa pode ser selecionada por vez');
-  } else {
+  if (tarefaSelecionada.length >= 1) {
+    tarefaSelecionada[0].classList.remove('selected');
+    event.target.classList.add('selected');
+  } else if (event.target.classList.contains('selected') === false) {
     event.target.classList.add('selected');
   }
-  console.log(tarefaSelecionada);
 }
 
 function criaTarefa() {
